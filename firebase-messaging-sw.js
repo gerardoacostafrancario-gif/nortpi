c571f95023e253c43f7570e694e78728a11b251b
gerardoacostafrancario-gif <gerardoacostafrancario@gmail.com>
2026-04-17 10:50:26 -0300

    Create firebase-messaging-sw.js

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,22 @@
+importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+  apiKey: "AIzaSyAdRnQQ-ZlZnZLEqbH0rNc5CExwWxMkVdE",
+  authDomain: "puerta-a-puerta-dc67c.firebaseapp.com",
+  projectId: "puerta-a-puerta-dc67c",
+  storageBucket: "puerta-a-puerta-dc67c.firebasestorage.app",
+  messagingSenderId: "685654039640",
+  appId: "1:685654039640:web:28f9f623c7642a4ef87e2d"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  console.log('Notificación en background:', payload);
+  const { title, body, icon } = payload.notification;
+  self.registration.showNotification(title, {
+    body,
+    icon: icon || '/icono.png'
+  });
+});
